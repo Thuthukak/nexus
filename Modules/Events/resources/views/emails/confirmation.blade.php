87d4a61d679fe9920f5416683b8292ada4bc5487
Thuthuka <thuthukakhoza@gmail.com>
2026-06-16 19:24:07 +0200

    fix(Events): reverted file back to previous commit

--- Modules/Events/resources/views/emails/confirmation.blade.php
+++ Modules/Events/resources/views/emails/confirmation.blade.php
@@ -64,7 +64,7 @@
           <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#1E3A5F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="flex-shrink:0;vertical-align:middle;">
             <path d="M2 9a3 3 0 0 1 0 6v2a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2a3 3 0 0 1 0-6V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
           </svg>
-          Order Reference: <strong>{{ $order->reference }}</strong>
+          Order Reference:  <strong>{{ $order->reference }}</strong>
         </span>
 
       </div>
